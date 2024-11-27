5fbeb4b875efbb674dc44ed7b4823f16f02619c1
Lina Cardozo <linacardozo@MacBook-Pro-de-Lina.local>
2024-11-27 15:28:51 -0500

    add files config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: "#f5741c",
+        darkBg: "#131424",
+      },
+      backgroundImage: {
+        "gradient-cover":
+          "linear-gradient(90.21deg, rgba(5, 71, 78, 1) 0%, rgba(24, 51, 56, 0.8) 50%, rgba(18, 42, 46, 0.6) 100%)",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
